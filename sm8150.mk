8afdbcf54a5cb5c1ea73b2fa4ceb93e537412d85
ilikestalker <37273872+ilikestalker@users.noreply.github.com>
2019-12-31 04:32:00 -0800

    Update sm8150.mk

--- sm8150.mk
+++ sm8150.mk
@@ -29,7 +29,6 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
 
 # Properties
 -include $(LOCAL_PATH)/common-props.mk
